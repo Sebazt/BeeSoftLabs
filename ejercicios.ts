14b4a077b27a26d3df3f8365b4b19c2d51e19268
Sebazt <juan.londono29@udea.edu.co>
2022-05-05 18:03:56 -0500

    Exercises task 3 done

---
+++ ejercicios.ts
@@ -0,0 +1,41 @@
+/* const addNum = function(a:number, b:number):number{
+  let total = a + b;
+  return total
+}
+console.log("El total es "+ addNum(20, 20)) */
+
+//esta función le estoy pasando un valor desde la función y otro al ser llamada y funciona.
+/* const presentation = (myName:string="Eren", myAge:number =28):string =>{
+  let NameComplete = `Hola, mi nombre es: ${myName} y tengo ${myAge} de edad`
+  return NameComplete;
+}
+
+console.log(presentation()); 
+console.log(presentation("Sebazt", 28)); */
+
+
+//Arrow Functions
+/* const greeting = (myName_2:string,) =>{
+  console.log(`This is a example, Hi ${myName_2}`)
+}
+greeting("JuanSebastian") */
+
+
+//retorno en una sola linea
+/* const Sum = (num_1:number,num_2:number):number=> num_1 * num_2;
+console.log(Sum(2,10)); */
+
+
+/* const ArrayNumber:number[] = [2,3,4,5,10]
+ArrayNumber.forEach((numer,index)=>{
+console.log(`${numer} se encuentra en el indice ${index}`)
+}) */
+
+
+//function recursiva
+/* const functionRecursiva = (a:number):number=>{
+  if(a<5) return a
+  console.log(a)
+  return functionRecursiva(a-1)
+}
+functionRecursiva(8) */
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,103 @@
-const addNum = function(a:number, b:number):number{
-  let total = a + b;
-  return total
+//example for TS
+/* const promesa  = new Promise ((resolve:Function, reject:Function)=>{
+  if(true){
+    setTimeout (()=>{
+        resolve("resolve retorna que todo esta bien y es el ultimo console.log ya que es una operación asincronca simulada por el setTimeOut()")
+    },2000)
+  }else{
+    reject(Error("Reject me devuelve el error, algo salio mal"))
+  }
+})
+
+promesa
+  .then((resolve) =>{
+    console.log(resolve)
+  }).catch((error)=>{
+    console.log(error)
+  })
+ */
+
+
+const exercise_1 = () => {
+  const myPromise = Promise.resolve(1);
+  setTimeout(() => {
+    myPromise.then((value) => {
+      console.log("---------------Exercise 1----------------");
+      console.log(value); // log: 1
+    });
+
+  }, 500);
+};
+exercise_1();
+
+
+const exercise_2 = (value:number) =>{
+  const myPromise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(value)  
+    },1000)
+})
+
+myPromise.then((value) => {
+  console.log("---------------Exercise 2----------------");
+ console.log(value)  // log: 2
+})}
+exercise_2(2);
+
+
+
+
+const exercise_3 = (value:number)=>{
+const myPromise = new Promise((resolve:Function, reject:Function) => {
+  setTimeout(() => {
+    resolve(value);
+  }, 1500);
+   
+})
+
+myPromise
+  .then((value) => {
+    console.log("---------------Exercise 3----------------");
+    console.log(value);
+     
+  })
+  .catch((value) => {
+   console.log(value + 2) // log: 4
+  })
 }
-console.log("El total es "+ addNum(20, 20))
+exercise_3(4);
 
-//esta función le estoy pasando un valor desde la función y otro al ser llamada y funciona.
-const presentation = (myName:string="Eren", myAge:number =28):string =>{
-  let NameComplete = `Hola, mi nombre es: ${myName} y tengo ${myAge} de edad`
-  return NameComplete;
+const exercise_4 = ()=>{
+  const myPromise = new Promise((resolve:Function, reject:Function) => {
+    setTimeout(() => {
+      reject("Error: Operation Failed");  
+    }, 2000);
+ 
+})
+
+myPromise
+  .catch((err) => {
+     console.log("---------------Exercise 4----------------");
+   console.log(err) // log: 'Error: Operation Failed'
+  })
 }
-
-console.log(presentation()); 
-console.log(presentation("Sebazt", 28));
+exercise_4()
 
 
-//Arrow Functions
-const greeting = (myName_2:string,) =>{
-  console.log(`This is a example, Hi ${myName_2}`)
+
+const exercise_5 = () =>{
+  const callback1 = (value: string) => console.log("Esto es un texto:", value);
+  const callback2 = (value: any) => console.log("Esto es un numero:", value);
+
+  const myPromise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(2);
+    }, 2500);
+  });
+
+  myPromise.then((value) => {
+    console.log("---------------Exercise 5----------------");
+    callback2(value);
+  }); // log: Esto es un numero: 2`
 }
-greeting("JuanSebastian")
-
-
-//retorno en una sola linea
-const Sum = (num_1:number,num_2:number):number=> num_1 * num_2;
-console.log(Sum(2,10));
-
+exercise_5()
